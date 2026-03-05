0fcc9d52ba4aecd26fb672f0a5c96654cfa367c7
Rafadex42 <rochamiguelbruno@gmail.com>
2026-03-05 17:07:27 +0000

    First question

--- first_quiz.py
+++ first_quiz.py
@@ -1,12 +1,22 @@
-print("Welcome to Bruno Rocha's Python Quiz!")
-
 name = input("Please enter your name: ")
 
+print(f"{name}, welcome to Bruno Rocha's Python Quiz!")
+
+#play = input(name + ", do you want to play? (yes/no): ")
 play = input(f"{name}, do you want to play? (yes/no): ")
 
-if play.lower() == "yes":
-    print("Let's play!")
+if play.lower() != "yes":
+    print("Maybe next time, see you later!")
+    quit()
 else:
-    print("Maybe next time!")
+    print("Let's play, it will be fun!")
 
 score = 0
+
+answer = input("What does CPU stand for? ")
+if answer.lower() == "central processing unit":
+    print("Correct!")
+    score += 1
+else:
+    print("Incorrect!")
+        
