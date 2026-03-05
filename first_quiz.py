3cfb8583d7de2f34dfe47fee1d5f1caa3e07c5b5
Rafadex42 <rochamiguelbruno@gmail.com>
2026-03-05 17:43:44 +0000

    while loop added

--- first_quiz.py
+++ first_quiz.py
@@ -8,15 +8,55 @@ play = input(f"{name}, do you want to play? (yes/no): ")
 if play.lower() != "yes":
     print("Maybe next time, see you later!")
     quit()
-else:
-    print("Let's play, it will be fun!")
 
-score = 0
 
-answer = input("What does CPU stand for? ")
-if answer.lower() == "central processing unit":
-    print("Correct!")
-    score += 1
-else:
-    print("Incorrect!")
-        
+
+while play.lower() == "yes":
+
+    score = 0
+
+    #1
+    answer = input("What does CPU stand for? ")
+    if answer.lower() == "central processing unit":
+        print("Correct!")
+        score += 1
+    else:
+        print("Incorrect!")
+
+    #2
+    answer = input("What does RAM stand for? ")
+    if answer.lower() == "random access memory":
+        print("Correct!")
+        score += 1
+    else:
+        print("Incorrect!")
+
+    #3
+    answer = input("What does GPU stand for? ")
+    if answer.lower() == "graphics processing unit":
+        print("Correct!")
+        score += 1
+    else:
+        print("Incorrect!")
+
+    #4
+    answer = input("What does PSU stand for? ")
+    if answer.lower() == "power supply unit":
+        print("Correct!")
+        score += 1
+    else:
+        print("Incorrect!")
+
+    #5
+    answer = input("What does HDD stand for? ")
+    if answer.lower() == "hard disk drive":
+        print("Correct!")
+        score += 1
+    else:
+        print("Incorrect!")
+
+    print(f"{name}, you got {score} out of 5 questions correct!")
+    print(f"Final score: {score/5 * 100:.2f}%")
+
+    play = input("Do you want to play again? (yes/no): ") 
+
