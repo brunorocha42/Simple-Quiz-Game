ef3fbf1586b476730f0672d5222c0581eb3a5a75
Rafadex42 <rochamiguelbruno@gmail.com>
2026-03-05 18:45:15 +0000

    dictionary creation

---
+++ questions.py
@@ -0,0 +1,34 @@
+perguntas ={
+    "Short Answer": {
+        "Hardware": {
+            "What does CPU stand for? ": "central processing unit",
+            "What does RAM stand for? ": "random access memory",
+            "What does GPU stand for? ": "graphics processing unit",
+            "What does PSU stand for? ": "power supply unit",
+            "What does HDD stand for? ": "hard disk drive"
+        },
+        "General Culture": {
+            "What is the capital of France? ": "Paris",
+            "What is the largest planet in our solar system? ": "Jupiter"
+        },
+        "Python":{
+            "What is the keyword used to define a function in Python? ": "def",
+            "What data type is used to store text in Python? ": "str",
+            "What symbol is used to comment a single line in Python? ": "#"
+        }
+    },
+    "TrueOrFalse": {
+        "Hardware": {
+            "The CPU is the brain of the computer. ": True,
+            "RAM is a type of permanent storage. ": False
+        },
+        "General Culture": {
+            "The Great Wall of China is visible from space. ": False,
+            "The Earth is flat. ": False
+        },
+        "Python":{
+            "Python is a high-level programming language. ": True,
+            "The 'print' function is used to display output in Python. ": True
+        }
+    }
+}
